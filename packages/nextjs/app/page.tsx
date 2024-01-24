667db9eadbd03daf65351d51d37687921de3ea40
sophiahdew <sophiahdew@gmail.com>
2024-01-24 15:23:56 -0600

    new ui edit

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
           <h1 className="text-center mb-8">
-            <span className="block text-2xl mb-2">Welcome to</span>
+            <span className="block text-2xl mb-2">Welcome to my new app</span>
             <span className="block text-4xl font-bold">Go Fund Me</span>
           </h1>
           <p className="text-center text-lg">
