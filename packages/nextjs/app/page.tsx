96024093b12355b9684fafe6bf218fa3256588ee
sophiahdew <sophiahdew@gmail.com>
2024-01-31 16:03:59 -0600

    tinkers

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
         <div className="px-5">
           <h1 className="text-center mb-8">
             <span className="block text-2xl mb-2">Welcome to my new app</span>
-            <span className="block text-4xl font-bold">Go Fund Me</span>
+            <span className="block text-4xl font-bold">Test my stuff</span>
           </h1>
           <p className="text-center text-lg">
             Get started by editing{" "}
